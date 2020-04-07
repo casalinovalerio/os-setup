bd5c1742a96fac32bf6ace4ad0a89bc1bbdd50a0
Valerio Casalino <casalinovalerio.cv@gmail.com>
2020-04-07 19:03:49 -0400

    Changed script to fit the run-as-root decision

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,6 +23,9 @@ err() { errmsg "$1"; return 1; }
 [ $EUID -ne 0 ] && errmsg "Please, run as root" && exit 1
 
 ### Functions
+# Installation wrapper
+installpkg() { for i in $@; do command -v "$i" > /dev/null | apt -y install "$i"; done || err "Failed to install $@"; }
+
 # Apt mirror:// method to get apt choose the best mirror
 setMirrors() {
   printf "deb mirror://mirrors.ubuntu.com/mirrors.txt %s main restricted universe multiverse" "$UBUNTU_CODENAME" | tee /etc/apt/sources.list
@@ -35,53 +38,52 @@ setMirrors() {
 installNerdFonts() {
   out=$( mktemp -d )
   git clone https://github.com/ryanoasis/nerd-fonts.git "$out"
-  /usr/bin/env bash -c "$out/install.sh --complete" && rm -rf "$out" || war "Fonts not installed correctly"
+  /usr/bin/env bash -c "$out/install.sh --complete" || err "Fonts not installed correctly"
+  rm -rf "$out"
 }
 
 # Zsh workflow
 installZsh() {
-  sudo apt -y install zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel9k || err "zsh not installed"
-  cp ./resources/.zshrc "$HOME"
+  installpkg zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel9k || err "zsh workflow not installed"
+  for i in /home/*; do cp ./resources/.zshrc "$i"; done
 }
 
 # Use alacritty.yml in the repository
 installAlacritty() {
-  sudo add-apt-repository "ppa:mmstick76/alacritty"
-  sudo apt -y install alacritty || err "Unable to install alacritty"
-  mkdir -p "$HOME/.config/alacritty"
-  cp ./resources/alacritty.yml "$HOME/.config/alacritty"
+  add-apt-repository "ppa:mmstick76/alacritty" && installpkg alacritty || err "Unable to install alacritty"
+  for i in /home/*; do mkdir -p "$i/.config/alacritty" && cp ./resources/alacritty.yml "$i/.config/alacritty"; done
 }
 
 # Use xterm as default?
 installXterm() {
-  sudo apt -y install xterm || err "Unable to install xterm"
-  cp ./resources/.Xresources "$HOME" && printf "Restart (or logout) required"
+  installpkg xterm || err "Unable to install xterm"
+  for i in /home/*; do cp ./resources/.Xresources "$i"; done && mss "Restart (or logout) required"
 }
 
 # Visual Studio Code installation
 installVScode() {
-  sudo apt -y install apt-transport-https
+  installpkg apt-transport-https
   curl -s "https://packages.microsoft.com/keys/microsoft.asc" | gpg --dearmor > /tmp/packages.microsoft.gpg
-  sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /usr/share/keyrings/
-  sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-  sudo apt -y update && sudo apt -y install code || war "Unable to install VScode"
+  install -o root -g root -m 644 /tmp/packages.microsoft.gpg /usr/share/keyrings/
+  sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  apt -y update && installpkg code || err "Unable to install VScode"
 }
 
 # xfce4 with some customizations
 installXfce() {
-  dpkg -l | grep xfce4-session || { sudo apt -y install xfce4 || err "Couldn't install xfce4"; }
+  dpkg -l | grep xfce4-session || { installpkg xfce4 || err "Couldn't install xfce4"; }
   xfce4-panel-profiles load ./resources/xfce4/panel.tar.bz2
-  cp ./resources/xfce4/xfce4-keyboard-shortcuts.xml "$HOME/.config/xfce4/xfce-perchannel-xml/"
+  for i in /home/*; do cp ./resources/xfce4/xfce4-keyboard-shortcuts.xml "$i/.config/xfce4/xfce-perchannel-xml/"; done
 }
 
 # Commands from https://brave-browser.readthedocs.io/en/latest/installing-brave.html#linux
 installBrave() { 
-  sudo apt -y install apt-transport-https curl || err "Failed to get pre-requisites for brave"
-  curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || err "Failed to add brave gpg signature"
-  echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list || err "Failed to add brave to repository sources"
-  sudo apt -y update && sudo apt -y install brave-browser || err "Failed to apt install brave"
+  installpkg apt-transport-https curl || err "Failed to get pre-requisites for brave"
+  curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || err "Failed to add brave gpg signature"
+  echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list || err "Failed to add brave to repository sources"
+  apt -y update && installpkg brave-browser || err "Failed to apt install brave"
   # Enable hardware acceleration
-  cp ./resources/chromium-flags.conf "$HOME/.config/brave-flags.conf"
+  for i in /home/*; do cp ./resources/chromium-flags.conf "$i/.config/brave-flags.conf"; done
 }
 
 ### Actual script
@@ -122,8 +124,8 @@ esac
 dialog --clear --backtitle "os-setup" --infobox "Setting up web browser" 15 70 && sleep 2
 case "$webBrowser" in
   "brave") clear; installBrave ;;
-  "firefox") clear; sudo apt -y install firefox ;;
-  "chromium") clear; sudo apt -y install chromium; cp ./resources/chromium-flags.conf "$HOME/.config/" ;;
+  "firefox") clear; installpkg firefox ;;
+  "chromium") clear; installpkg chromium; for i in /home/*; do cp ./resources/chromium-flags.conf "$i/.config/"; done ;;
   *) ;;
 esac
 
